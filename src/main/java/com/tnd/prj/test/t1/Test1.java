8ce6c4420c4e16c8f355c016f1adf8c7d73b6266
JinseokJang91 <hya2089@gmail.com>
2024-11-09 19:43:36 +0900

    feature/20241109 first commit

--- src/main/java/com/tnd/prj/test/t1/Test1.java
+++ src/main/java/com/tnd/prj/test/t1/Test1.java
@@ -2,10 +2,6 @@ package com.tnd.prj.test.t1;
 
 public class Test1 {
     public static void main(String[] args) {
-        // this is Test1
-
-        // this is Test1 repeat
-
-        // this is rebase test feature/20241109
+        // this is first
     }
 }
