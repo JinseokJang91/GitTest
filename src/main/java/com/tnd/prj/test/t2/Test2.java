3e047202e1ddecd24a24ef30913f5d6ff235919e
Jinseok Jang <hya2089@gmail.com>
2024-11-29 23:51:16 +0900

    20241129 second commit

--- src/main/java/com/tnd/prj/test/t2/Test2.java
+++ src/main/java/com/tnd/prj/test/t2/Test2.java
@@ -2,29 +2,6 @@ package com.tnd.prj.test.t2;
 
 public class Test2 {
     public static void main(String[] args) {
-        // this is hotfix first
-
-        // feature/20241121 modified this file.
-    	// feature/test modified this file.
-    	
-    	String test1 = "test11111";
-    	String test2 = "test22222";
-    	String feature20241121 = "modified";
-    }
-    
-    public void test() {
-    	System.out.println("feature/test modified this file");
-    }
-
-    public static void test01() {
-        System.out.println("test01");
-    }
-
-    public static void test02() {
-        System.out.println("test02");
-    }
-
-    public static void test03() {
-        System.out.println("test03 feature/20241127");
+        String test2 = "test2";
     }
 }
